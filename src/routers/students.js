4b74746df211775a4ad40fbcec270b80917b0412
Serhii Tyron <mestn1y@hotmail.com>
2024-11-13 00:23:37 +0200

    add middleware for auth

--- src/routers/students.js
+++ src/routers/students.js
@@ -9,6 +9,7 @@ import {
 } from '../controllers/controllers.js';
 import { ctrlWrapper } from '../utils/ctrlWrapper.js';
 import { validateBody } from '../middlewares/validateBody.js';
+import { authenticate } from '../middlewares/authenticate.js';
 import {
   createStudentSchema,
   updateStudentSchema,
@@ -16,6 +17,8 @@ import {
 import { isValidId } from '../middlewares/isValid.js';
 
 const router = Router();
+
+router.use(authenticate);
 
 router.get('/', ctrlWrapper(getStudentsController));
 
